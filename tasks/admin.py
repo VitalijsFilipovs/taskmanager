ea56dd31f759e0da7c65a8aed945169fe8ed6edd
VitalijsFilipovs <elektronnajapo4ta@gmail.com>
2025-07-15 18:44:15 +0300

    Добавил инлайн SubTask к Task в админке

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Task, SubTask
 
-# Register your models here.
+
+class SubTaskInline(admin.TabularInline):
+    model = SubTask
+    extra = 1  # количество пустых форм
+
+
+class TaskAdmin(admin.ModelAdmin):
+    inlines = [SubTaskInline]
+
+
+admin.site.register(Task, TaskAdmin)
+admin.site.register(SubTask)
